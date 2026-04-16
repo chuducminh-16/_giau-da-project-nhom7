540b9e435435b8dc80a6419a4d24df9ec8f27462
chử đức minh <chuducminnh@gmail.com>
2026-04-16 15:53:50 +0700

    commit5 giao diện thêm

--- auction-client/src/main/java/com/auction/client/HelloApplication.java
+++ auction-client/src/main/java/com/auction/client/HelloApplication.java
@@ -13,8 +13,8 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(
                 HelloApplication.class.getResource("/hello-view.fxml")
         );
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        stage.setTitle("Đấu giá");
         stage.setScene(scene);
         stage.show();
     }
