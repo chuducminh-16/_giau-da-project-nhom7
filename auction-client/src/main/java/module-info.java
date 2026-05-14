f9b44e526d72901bbc9d7a08aab942c06e344607
tvhiep13042007 <tvhiep13042007@gmail.com>
2026-05-14 10:06:37 +0700

    Mở Đầu:))

--- auction-client/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.auction.client {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.web;
-
-    opens com.auction.client to javafx.fxml;
-    exports com.auction.client;
-}
