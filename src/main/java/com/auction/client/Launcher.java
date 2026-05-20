b9e37b6fc06ef16fef6e351e265eb931d177d217
chử minh <chuducminnh@gmail.com>
2026-05-20 14:44:30 +0700

    fix zoom luôn full màn hình

--- src/main/java/com/auction/client/Launcher.java
+++ src/main/java/com/auction/client/Launcher.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 
 public class Launcher {
     public static void main(String[] args) {
+        System.setProperty("prism.allowhidpi", "true");
         Application.launch(AuctionClientApp.class, args);
     }
 }
